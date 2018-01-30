34c6d749e01430a543f1a350a9bc54c08ecf944c
Mika Korpela <mika.1.korpela@nokia.com>
2018-01-30 13:13:17 +0200

    Go implementation added with multi-container configuration.

---
+++ go-hello/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "io"
+import "net/http"
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8000", nil)
+}
